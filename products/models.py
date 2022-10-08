b4e48478676b3abbfbcf517cbc76347fcc11333d
kwalczak80 <christopher.walczak@gmail.com>
2022-10-08 16:09:45 +0000

    Add meta class to correctly display the categories name in the admin panel

--- products/models.py
+++ products/models.py
@@ -5,6 +5,10 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
