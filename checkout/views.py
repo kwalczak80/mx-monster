2b535df72b9f16315602a44114e5277178e51ae2
kwalczak80 <christopher.walczak@gmail.com>
2022-10-22 12:58:58 +0000

    Add form validation before submission

--- checkout/views.py
+++ checkout/views.py
@@ -26,14 +26,17 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51LvQyUFP2Wzh63aNfomT865EMDtwqEfIbDoU5DFA8BYvVIhtbN25ym9byx0pwQmal7vVD6MnKKmiLew3dZ5a40dX00TzKCMnsz',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
