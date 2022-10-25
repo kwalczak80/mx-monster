a9bbd2d797c098d48a5237692f3e7cc5d3c9628c
kwalczak80 <christopher.walczak@gmail.com>
2022-10-25 19:43:26 +0000

    Change custom_clearable_file location

--- products/widgets.py
+++ products/widgets.py
@@ -1,6 +1,8 @@
 """[widgets for products]"""
 from django.forms.widgets import ClearableFileInput
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+
 
 
 class CustomClearableFileInput(ClearableFileInput):
@@ -8,7 +10,5 @@ class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('')
-    template_name = (
-        'products/custom_widget_templates/custom_clearable_file_input.html'
-        )
+    template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
 
