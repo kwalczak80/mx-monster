c07efc61fc0888124d6e013cf19e1c195f908578
kwalczak80 <christopher.walczak@gmail.com>
2022-11-05 14:22:13 +0000

    Change the layout of  404 error page

--- mx_monster/settings.py
+++ mx_monster/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
+# DEBUG = False
 
 ALLOWED_HOSTS = ['mx-monster.herokuapp.com', 'localhost']
 
