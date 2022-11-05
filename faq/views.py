abe3ea9754b46b492db53e35a0a3afc1aee5dda3
kwalczak80 <christopher.walczak@gmail.com>
2022-11-05 15:24:57 +0000

    Update views and urls for faq application. Create faq.html file

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
-# Create your views here.
+
+def faq(request):
+    """ Frequently asked questions """
+    return render(request, 'faq/faq.html')
