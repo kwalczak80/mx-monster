44428c84c38ba870bfc08721888747fb0aa4e74a
kwalczak80 <christopher.walczak@gmail.com>
2022-11-06 19:21:22 +0000

    Migrate newsletter model to the database

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,4 +1,10 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+
+from .models import NewsletterUser
+from .forms import NewsletterUserSignUpForm
 
 
 def newsletter_subscribe(request):
@@ -12,4 +18,34 @@ def newsletter_signup(request):
     """
     A view to display the newsletter sign up page
     """
-    return render(request, 'newsletter/newsletter_signup.html')
+    form = NewsletterUserSignUpForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if NewsletterUser.objects.filter(email=instance.email).exists():
+            messages.warning(request,
+                             'Sorry, you have already subscribed to our newsletter!')
+        else:
+            messages.success(request,
+                             'Congratulations !! You have successfully subscribed to our  newsletter!')
+            instance.save()
+            subject = "Thank you for Joining our Newsletter"
+            from_email = settings.EMAIL_HOST_USER
+            to_email = [instance.email]
+            signup_message = """Welcome to MX Monster Newsletter, \
+                If you would like to unsubscribe visit \
+                    https://mx-monster.herokuapp.com/newsletter/unsubscribe/"""
+            send_mail(subject=subject,
+                      from_email=from_email,
+                      recipient_list=to_email,
+                      message=signup_message,
+                      fail_silently=False)
+
+    context = {
+        'form': form,
+    }
+
+    template = 'newsletter/newsletter_signup.html'
+
+    return render(request, template, context)
+
