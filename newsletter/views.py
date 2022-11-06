83e0c99a9f83d85a5166ac2bd21c616418a07c5a
kwalczak80 <christopher.walczak@gmail.com>
2022-11-06 18:46:38 +0000

    Add link to Subscribe button to redirect subscribe page

--- newsletter/views.py
+++ newsletter/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 
 def newsletter_subscribe(request):
     """
-    A view to display newsletter subscription page
+    A view to display the newsletter subscription page
     """
     return render(request, 'newsletter/newsletter_subscription.html')
+
+
+def newsletter_signup(request):
+    """
+    A view to display the newsletter sign up page
+    """
+    return render(request, 'newsletter/newsletter_signup.html')
