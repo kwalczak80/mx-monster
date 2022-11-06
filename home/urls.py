f647761c634cf9ce9e9df917f212920d0a6742de
kwalczak80 <christopher.walczak@gmail.com>
2022-11-06 13:46:45 +0000

    Create terms and contitions page. Create view and update urls

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
+"""
+Urls for home app
+"""
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('privacy', views.privacy, name='privacy'),
+    path('terms', views.terms, name='terms'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 
+
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
 
+
 def privacy(request):
     """
-    view to display Privacy Policy
+    A view to display Privacy Policy
     """
-    return render(request, 'privacy_page/privacy.html')
+    return render(request, 'privacy/privacy.html')
+
+
+def terms(request):
+    """
+    A view to display Terms and Conditions
+    """
+    return render(request, 'terms/terms.html')
