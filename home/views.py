564f4dd4c1dcb193a4af7030fb1c8328b2f694e7
kwalczak80 <christopher.walczak@gmail.com>
2022-11-06 13:31:34 +0000

    Create the privacy page to inform the user that the business complies with the law

--- home/views.py
+++ home/views.py
@@ -4,3 +4,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def privacy(request):
+    """
+    view to display Privacy Policy
+    """
+    return render(request, 'privacy_page/privacy.html')
