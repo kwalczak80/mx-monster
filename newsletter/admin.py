bc2382601d798be5fabd3d7297f58f321c463830
kwalczak80 <christopher.walczak@gmail.com>
2022-11-06 16:14:54 +0000

    Create class to display newsletter model in the admin panel

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,11 @@
+""" Admin newsleter """
 from django.contrib import admin
+from .models import NewsletterUser
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    """ Admin newsleter """
+    list_display = ('email', 'date_added',)
+
+
+admin.site.register(NewsletterUser, NewsletterAdmin)
