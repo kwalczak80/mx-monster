d2494eefe810381bde104fbba176d09ee390d1e6
kwalczak80 <christopher.walczak@gmail.com>
2022-11-07 18:36:33 +0000

    Update contact views to send email if the form is correct. Add relevant messages

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
+from django.core.mail import send_mail
+from django.conf import settings
+
+from .forms import ContactForm
 
 
 def contact(request):
-    """ A view to return the contact page """
-
-    return render(request, 'contact/contact.html')
+    """ A view to display the contact page """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        subject = request.POST['subject']
+        message = request.POST['message']
+        email = request.POST['email']
+        sender = "Contact request from" + " " + str(email)
+        msg_mail = str(message) + ", " + str(sender)
+        if form.is_valid():
+            send_mail(
+                subject,
+                msg_mail,
+                email,
+                [settings.DEFAULT_FROM_EMAIL, ]
+            )
+            form.save()
+            messages.success(request, 'Thank you. Your Email has been sent !!')
+            return redirect(reverse('contact'))
+        else:
+            messages.error(
+                request,
+                'An error occurred when trying to send your email message.\
+                 Please check your form.'
+            )
+    else:
+        form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact/contact.html', context)
