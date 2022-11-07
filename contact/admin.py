01da2e0fb6423652ddfb56e14a5f619ee018beb3
kwalczak80 <christopher.walczak@gmail.com>
2022-11-07 18:15:52 +0000

    Register contact app in the admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
+""" Admin panel for the contact app"""
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Admin class to display email subject and message
+    from the user
+    """
+    list_display = (
+        'email',
+        'subject',
+        'message'
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
