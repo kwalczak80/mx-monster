8c1db70139979b2f44222e3724d8ee6481a0eb47
kwalczak80 <christopher.walczak@gmail.com>
2022-11-09 19:23:11 +0000

    Migrate the review model to the databse

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
+""" Reviews model """
 from django.db import models
 
-# Create your models here.
+from profiles.models import UserProfile
+from products.models import Product
+
+
+class Review(models.Model):
+    """ Reviews model """
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    description = models.TextField(max_length=500, null=False,
+                                   blank=False)
+    review_date = models.DateTimeField(auto_now_add=True)
