4c39666d8535a65a2484e54b09a8d3927247e292
kwalczak80 <christopher.walczak@gmail.com>
2022-11-09 21:47:09 +0000

    Update product detail view display information about the product reviews. Add relavant imports

--- products/views.py
+++ products/views.py
@@ -3,7 +3,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-
+from reviews.forms import ReviewForm
+from reviews.models import Review
 from .models import Product, Category
 from .forms import ProductForm
 
@@ -64,9 +65,14 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    reviews = Review.objects.filter(product=product)
+    review_form = ReviewForm()
+    # product.save()
 
     context = {
         'product': product,
+        'reviews': reviews,
+        'review_form': review_form,
     }
 
     return render(request, 'products/product_detail.html', context)
