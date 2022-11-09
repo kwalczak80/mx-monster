3875074f3eaafb5b61d134a73a1438538353202a
kwalczak80 <christopher.walczak@gmail.com>
2022-11-09 20:15:52 +0000

    Create forms.py and add Review form class

---
+++ reviews/forms.py
@@ -0,0 +1,11 @@
+""" Reviews form """
+from django import forms
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    """ Form to add reviews of products """
+    class Meta:
+        """ Form meta """
+        model = Review
+        fields = ('description',)
