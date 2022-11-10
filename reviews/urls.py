e6070ffd7c52ea9b4453739171eda3d1c1a01f95
kwalczak80 <christopher.walczak@gmail.com>
2022-11-10 18:30:02 +0000

    Update urls.py to add path to edit review

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path("add_review/<product_id>", views.add_review, name="add_review"),
+    path("edit_review/<int:review_id>", views.edit_review, name="edit_review"),
 ]
