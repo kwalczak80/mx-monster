f7e254fa234f0358347c34ba7908c5c9b4c1fcfe
kwalczak80 <christopher.walczak@gmail.com>
2022-11-12 21:01:15 +0000

    Create staff model

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,20 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Staff(models.Model):
+    """
+    The Staff model
+    """
+    name = models.CharField(max_length=100, default='')
+    description = models.TextField(default='')
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50, default='')
+    photo = models.ImageField(null=True, blank=True)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        """
+        Return the staff name
+        """
+        return self.name
