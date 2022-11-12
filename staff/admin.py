1028ca37ac76686245cdd4d14470b8c15a2b9245
kwalczak80 <christopher.walczak@gmail.com>
2022-11-12 21:05:27 +0000

    Create class to display Staff info in the admin panel

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Staff
 
-# Register your models here.
+
+class StaffAdmin(admin.ModelAdmin):
+    """
+    Admin class for Staff to display
+    specific fields in the admin panel
+    """
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Staff, StaffAdmin)
