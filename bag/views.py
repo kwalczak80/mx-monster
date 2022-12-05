31452e52384e757918473d4e052221bee3f156ac
kwalczak80 <christopher.walczak@gmail.com>
2022-12-05 18:59:15 +0000

    Fix the issue when the user was able to adjust the bag quantity with the number exceeding the product stock

--- bag/views.py
+++ bag/views.py
@@ -92,34 +92,47 @@ def adjust_bag(request, item_id):
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
-
-    if size:
-        if quantity > 0:
-            bag[item_id]['items_by_size'][size] = quantity
-            messages.success(
-                request, (
-                    f'Updated size {size.upper()} {product.name} quantity to '
-                    f'{bag[item_id]["items_by_size"][size]}'
-                )
-            )
-        else:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(
-                request, (
-                    f'Removed size {size.upper()} {product.name} from '
-                    f'your bag'
-                )
-            )
+    if quantity > product.stock:
+        messages.error(request, (
+                    f'Currently, we have only {product.stock} '
+                    f'items of the {product.name} in stock. '
+                    f'Please enter the correct quantity.'
+                    )
+        )
     else:
-        if quantity > 0:
-            bag[item_id] = quantity
-            messages.success(
-                request, f'Updated {product.name} quantity to {bag[item_id]}')
+        if size:
+            if quantity > 0:
+                bag[item_id]['items_by_size'][size] = quantity
+                messages.success(
+                    request, (
+                        f'Updated size {size.upper()} '
+                        f'{product.name} quantity to '
+                        f'{bag[item_id]["items_by_size"][size]}'
+                    )
+                )
+            else:
+                del bag[item_id]['items_by_size'][size]
+                if not bag[item_id]['items_by_size']:
+                    bag.pop(item_id)
+                messages.success(
+                    request, (
+                        f'Removed size {size.upper()} {product.name} from '
+                        f'your bag'
+                    )
+                )
         else:
-            bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
+            if quantity > 0:
+                bag[item_id] = quantity
+                messages.success(
+                    request, (
+                        f'Updated {product.name} '
+                        f'quantity to {bag[item_id]}')
+                )
+            else:
+                bag.pop(item_id)
+                messages.success(request, (
+                    f'Removed {product.name} from your bag')
+                )
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
