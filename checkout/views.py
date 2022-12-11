15cb34354b567aebe3cc218856658238a8f604ad
kwalczak80 <christopher.walczak@gmail.com>
2022-12-11 10:19:01 +0000

    Add functionality to decrement product stock quantity after a successful purchase

--- checkout/views.py
+++ checkout/views.py
@@ -152,6 +152,11 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+    line_items = order.lineitems.all()
+    for item in line_items:
+        product = item.product
+        product.stock -= item.quantity
+        product.save()
 
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
